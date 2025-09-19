5c3d3ca3df19153c0e78ec72ee9a53f255fa14b6
Saketh <psakethvishnu@gmail.com>
2025-09-19 18:03:32 +0530

    critical Fix(dotenv) added dotenv

--- connectDB.py
+++ connectDB.py
@@ -1,10 +1,14 @@
+
 def connectDB():
+    from dotenv import load_dotenv
     import mysql.connector as mysql
+    import os
+    load_dotenv()
     DB = mysql.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="test"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     cursor = DB.cursor()
     print("Database connected successfully!")
